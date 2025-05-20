f83a3ec43e37c6f78cd03ad935407a8a6ad29dc7
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-05-21 01:38:22 +0600

    DB connect

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
+const connectDB = require("./server/config/db")
 
 const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+
 
 const app = express();
 
+
 const PORT = 5000 || process.env.PORT;
 
-app.get("", (req, res) => {
-    res.send("Hello from 5000")
-})
+// connect DB 
+connectDB();
+
+app.use(express.static("public"));
+
+// templating engine 
+app.use(expressLayout);
+app.set("layout", "./layouts/main")
+app.set("view engine", "ejs");
+
+
+app.use("/", require("./server/routes/main"))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
